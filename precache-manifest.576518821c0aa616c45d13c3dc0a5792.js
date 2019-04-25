bb062636dde71bb375e56cc253ac7bdb509bd721
cboetto <cmitchellboetto@gmail.com>
2019-04-25 13:10:32 -0400

    Updates

--- precache-manifest.576518821c0aa616c45d13c3dc0a5792.js
+++ precache-manifest.576518821c0aa616c45d13c3dc0a5792.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/static/media/lima.343e994c.png"
   },
   {
-    "revision": "bd7d8fd21054979964a4",
-    "url": "/static/css/main.5c8fadd4.chunk.css"
+    "revision": "0f145b1720cce2cb6956",
+    "url": "/static/css/main.cf943aec.chunk.css"
   },
   {
     "revision": "fdfcfda2d9b1bf31db52",
@@ -16,24 +16,24 @@ self.__precacheManifest = [
     "url": "/static/js/2.abdbe8da.chunk.js"
   },
   {
-    "revision": "527d47949a8413c8e92fda26fa3bf361",
-    "url": "/static/media/plum.527d4794.png"
-  },
-  {
     "revision": "6117c37c2b96a6fb41e04fc03257cc59",
     "url": "/static/media/coral.6117c37c.png"
   },
   {
-    "revision": "bd7d8fd21054979964a4",
-    "url": "/static/js/main.bd7d8fd2.chunk.js"
+    "revision": "527d47949a8413c8e92fda26fa3bf361",
+    "url": "/static/media/plum.527d4794.png"
   },
   {
-    "revision": "341bd5c23125c3ed0becd7c75353ef03",
-    "url": "/static/media/portfolioselect.341bd5c2.png"
+    "revision": "0f145b1720cce2cb6956",
+    "url": "/static/js/main.0f145b17.chunk.js"
   },
   {
     "revision": "8281c90f3e387576ee930e0f515e41c7",
     "url": "/static/media/skillsselect.8281c90f.png"
+  },
+  {
+    "revision": "341bd5c23125c3ed0becd7c75353ef03",
+    "url": "/static/media/portfolioselect.341bd5c2.png"
   },
   {
     "revision": "0ccca831f7030a4aa1f3f362349d38a0",
@@ -44,7 +44,7 @@ self.__precacheManifest = [
     "url": "/static/media/bigfatcellphone.f4c744b5.jpg"
   },
   {
-    "revision": "69b72f2c7373d4a07ddc78dd5fb8f394",
+    "revision": "4ccb83f0c2b3cc6bdbca3d18e2f1c384",
     "url": "/index.html"
   }
 ];
